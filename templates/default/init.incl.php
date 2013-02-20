37a10104286dddae01535847e37c904f905272d0
Chris <chris@chris.(none)>
2013-02-20 19:11:52 +0000

    include templates folder to commit

---
+++ templates/default/init.incl.php
@@ -0,0 +1,10 @@
+<?php
+/* Base template for creating a template for
+unicenta-web-frontend.
+
+All constants for a basic theme must be set (see start of this file). 
+This script is called to initalise the theme.	
+*/
+
+define('SITE_NAME', 'Unicenta Web Frontend');
+define('HEADER_IMAGE', 'default-header.jpg');
