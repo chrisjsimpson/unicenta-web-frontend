9eeb75a7b3635be71c40b6a28977675f7b12639f
Chris <chris@chris.(none)>
2013-02-23 23:13:51 +0000

    Ammended commit. Basic Dynamic Menu complete

--- includes/functions.incl.php
+++ includes/functions.incl.php
@@ -12,65 +12,76 @@ function load_header()
 
 function load_primary_navigation($dbc)
 {
-//	$categories = array(array());
-	$nav = '<nav>
-	<ul>';
+	$q_parentCategories = "
+SELECT NAME, ID FROM CATEGORIES WHERE
+PARENTID IS NULL";
+
+$r_parentCategories = mysqli_query($dbc, $q_parentCategories);
 
 
-	//Load categories 
-	$q = "SELECT NAME, ID, PARENTID FROM CATEGORIES";
-	$r = mysqli_query($dbc, $q);
-	
-	echo '<h2>Get all categoried</h2>'. $q;
+//Begin html list of categories
+echo "<ul>\n";
+/* Foreach parent category echo category parent and check for sub categories.
+    If a subcategory exists for the parent, echo this into the html list markup.
+    
+    Note- This is not a recursive solution, it only goes down three levels
+    (Parent = level1, subcategory 1 = level2,  subcategory2 = level3)
+*/
+while($category = mysqli_fetch_array($r_parentCategories))
+{
+  
+  echo "\t<li>" . $category['NAME'];
+  
+   //Get sub categories (if any)
+    $q_getFirstSubcategories = '
+    SELECT NAME, ID FROM CATEGORIES
+    WHERE PARENTID = "' . $category['ID'] . '"' ;
+     
+    $r_getFirstSubcategories = mysqli_query($dbc, $q_getFirstSubcategories);
 
-	//check each category for a parent before echoing
-	while($categories = mysqli_fetch_array($r, MYSQL_ASSOC))
-	{
-		//Check if has parent
-		if(!categoryHasParent($dbc, $categories['ID']))
+    if(mysqli_num_rows($r_getFirstSubcategories) > 0) //Echo first subcategory
+    {
+      echo "\n\t<ul>";
+      while($firstSubcat =  mysqli_fetch_array($r_getFirstSubcategories))
+      {
+	echo "\n\t\t<li>" . $firstSubcat['NAME']; 
+
+	load_subcategory($dbc, $firstSubcat['ID']);
+	 
+      }//End echo each subcategory
+      echo "</ul>\n";
+    }//End echo if a subcategory exists.
+    
+    echo "</li>\n";
+    
+}//Echo each category
+
+echo '</ul>';
+
+}//End load_primary_navigation($dbc)
+
+function load_subcategory($dbc, $id)
+{
+   $q = "SELECT NAME, ID FROM CATEGORIES WHERE
+		  PARENTID = '$id'";
+		  		  
+	    $r = mysqli_query($dbc, $q);
+	    
+	    if(mysqli_num_rows($r) > 0)
+	    {
+	    
+	      echo "\n\t<ul>";
+		while($subcategory = mysqli_fetch_array($r))
 		{
-			//Has no parent
-			echo 'here';
-			$category[] = $categories['NAME'];
-		}else{
-			echo 'has a parent';
+		  echo "\n\t<li>" . $subcategory['NAME'] . '</li>';
 		}
-	}//End echo each category
+	      echo "\n</ul>";
+	    }else{
+	      
+		echo "</li>";
+	    }
+}//End load_subcategory($dbc, $id)
 
-	print_r($category);
-
-	//Close navigation list & <nav> html5 tag
-
-	$nav .= '</ul></nav>';
-
-	
-
-}//End load_primary_navigation()
-
-function getCategoryParentName($dbc, $id)
-{
-	$q = "SELECT NAME FROM CATEGORIES WHERE ID = '$id'";
-
-	$r = mysqli_query($dbc, $q);
-
-	list($parentName) = mysqli_fetch_array($r);
-
-	return $parentName;
-}
-
-function categoryHasParent($dbc, $id)
-{
-	$q = "SELECT ID FROM CATEGORIES WHERE ID = '$id' AND
-		PARENTID IS NULL";
-	$r = mysqli_query($dbc, $q);
-	
-	if(mysqli_num_rows($r) == 0)
-	{
-	return true;
-	}else{
-	return false;
-	}
-}
 
 function load_footer()
 {
