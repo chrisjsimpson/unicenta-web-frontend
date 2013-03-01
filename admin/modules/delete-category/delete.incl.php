35acabeafb69a7a147f523b15fbb9ea6473abaa7
chrisjsimpson <chris.j.simpson@live.co.uk>
2013-03-01 21:04:30 +0000

    delete branch initialcom

---
+++ admin/modules/delete-category/delete.incl.php
@@ -0,0 +1,21 @@
+<?php
+/*
+This script takes the ID of a category, validates it and then deletes the category after confirming deletion.
+*/
+
+$categroryId ='x';
+$errors = array();
+
+if($confirmDelete)
+{
+	if(validCategoryId($dbc, $categoryId)
+	{
+
+	}//End only process valid category ids
+}else{//End delete category 
+
+echo "/n" . '
+	<form action="thispage+confirm"
+';
+
+}//End ask user to confirm deletion
